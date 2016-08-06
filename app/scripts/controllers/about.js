26d0de249ad4a01e5c0968f1862a4e9df5cb1e2b
harvinder singh <harvinder_singh@external.mckinsey.com>
2016-08-06 19:43:39 +0530

    First code commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name minicartApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the minicartApp
+ */
+angular.module('minicartApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name minicartApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the minicartApp
+ */
+angular.module('minicartApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
